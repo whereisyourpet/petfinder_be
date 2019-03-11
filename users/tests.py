cd41f205999ad56f93fa271c3bd7c857fac24843
yan_sy <yan_sy@126.com>
2019-03-11 09:29:56 +0800

    add demo test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import User
 
 # Create your tests here.
+
+
+class UserDemoTests(TestCase):
+    def test_demo(self):
+        new = User(username="new")
+        self.assertIs(new.gender, 0)
