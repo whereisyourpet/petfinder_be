62bbb7721f0030b2bc1fd2bedaa9ec587be6c772
yan_sy <yan_sy@126.com>
2019-03-11 16:56:25 +0800

    config StateAdmin

--- pets/admin.py
+++ pets/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import pet,state
+from .models import pet, state
+
+
+class StateAdmin(admin.ModelAdmin):
+    list_display = ('state_id', 'state_name')
+
 
 # Register your models here.
 admin.site.register(pet)
-admin.site.register(state)
+admin.site.register(state, StateAdmin)
