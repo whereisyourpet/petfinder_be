9bd9d1a6a6466e9246d998e7127862b2fc790e06
iblackholmes <846836299@qq.com>
2019-03-12 11:44:49 +0800

    modify views and add API

--- pets/urls.py
+++ pets/urls.py
@@ -4,10 +4,11 @@ from . import views
 app_name = 'pets'
 
 urlpatterns = [
-    path('get_pet_info', views.get_pet_info, name='get_pet_info'),
+    path('get_pets_of_user/', views.get_pets_of_user, name='get_pets_of_user'),
     path('publish_pet_information', views.publish_pet_information, name='publish_pet_information'),
     path('modify/', views.modify, name='modify'),
     path('delete/', views.delete, name='delete'),
-
-    path('get_recommand_pets', views.get_recommand_pets, name='get_recommand_pets'),
+    path('petfilter/', views.petfilter, name='petfilter'),
+    path('get_recommand_pets/', views.get_recommand_pets, name='get_recommand_pets'),
+    path('get_pet_info_from_id/', views.get_pet_info_from_id, name='get_pet_info_from_id'),
 ]
